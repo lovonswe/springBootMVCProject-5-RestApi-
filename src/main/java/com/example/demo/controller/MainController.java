64134fd62dd682fb133d2f08a3741bcaf6d61567
nahidswe <nahidswe@gmail.com>
2023-03-08 09:35:20 +0600

    add and update added

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Course;
@@ -46,5 +48,23 @@ public class MainController {
 		return courseRepo.findById(id);
 	}
 	
+	@PostMapping("/student/add")
+	public Student addStudent(@RequestBody Student student) {
+		return studentRepo.save(student);
+	}
+
+	@PostMapping("/course/add")
+	public Course addCourse(@RequestBody Course course) {
+		return courseRepo.save(course);
+	}
 	
+	@PostMapping("/student/update")
+	public Student updateStudent(@RequestBody Student student) {
+		return studentRepo.save(student);
+	}
+	
+	@PostMapping("/course/update")
+	public Course updateCourse(@RequestBody Course course) {
+		return courseRepo.save(course);
+	}
 }

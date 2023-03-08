f5502501f9d750122f4ff6165ccfd9a60ce3fda7
nahidswe <nahidswe@gmail.com>
2023-03-08 08:10:00 +0600

    find students and courses by id added in MainController

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Course;
@@ -25,12 +27,24 @@ public class MainController {
 	}
 	
 	@GetMapping("/student/all")
-	public List<Student> getStudents(){
+	public List<Student> getAllStudents(){
 		return studentRepo.findAll();
 	}
 	
 	@GetMapping("/course/all")
-	public List<Course> getCourses(){
+	public List<Course> getAllCourses(){
 		return courseRepo.findAll();
 	}
+	
+	@GetMapping("/student/{id}")
+	public Optional<Student> findStudentById(@PathVariable("id") int id) {
+		return studentRepo.findById(id);
+	}
+	
+	@GetMapping("/course/{id}")
+	public Optional<Course> findCourseById(@PathVariable("id") int id) {
+		return courseRepo.findById(id);
+	}
+	
+	
 }
